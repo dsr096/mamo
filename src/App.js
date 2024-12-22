00542d8fde6004c02c761995a5bf6f88f37d7161
dsr2004 <dsr0948@gmail.com>
2024-12-22 22:01:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/#/product" element={<Product />} />
+        <Route path="/#/contact" element={<Contact />} />
       </Routes>
     </HashRouter>
   );
